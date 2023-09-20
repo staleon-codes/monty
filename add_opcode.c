7eb54cb5d880a1f93106c159fba8c76cbb4b7c32
Peter McMillan <132352243+Steezcool@users.noreply.github.com>
2023-09-20 11:26:24 +0200

    Update

--- add_opcode.c
+++ add_opcode.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
- * op_add - adds the top two elements of the stack
+ * op_add - adds two elements of the stack
  * @stack: pointer to the top of the stack
  * @line_number: line number of the opcode
  **/
